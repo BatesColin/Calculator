d6b8c8cc429d3591a6dda63b5ea4f4fe85085d2a
Guest User <Guest@epicodus-118.local>
2018-05-30 16:46:11 -0700

    update calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
-var num1 = prompt("First Number");
-var num2 = prompt("Second Number");
-var normnum1 = parseInt(num1);
-var normnum2 = parseInt(num2);
-
-var add = function(normnum1, normnum2) {
-return normnum1 + normnum2;
+var add = function(number1, number2) {
+return number1 + number2;
 };
-var subtract = function(normnum1, normnum2) {
-return normnum1 - normnum2;
+var subtract = function(number1, number2) {
+return number1 - number2;
 };
-var multiply = function(normnum1, normnum2) {
-return normnum1 * normnum2;
+var multiply = function(number1, number2) {
+return number1 * number2;
 };
-var divide = function(normnum1, normnum2) {
-return normnum1 / normnum2;
+var divide = function(number1, number2) {
+return number1 / number2;
 };
 
-var addresult = add(normnum1, normnum2);
-alert("The sum is: " + addresult);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2)
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2)
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2)
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2)
+    $("#output").text(result);
+  });
+});
